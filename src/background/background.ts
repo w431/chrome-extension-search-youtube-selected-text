e0d6e1b45ad5d80ea1159c3a32500a357e474cbc
Wael Hamdan <whamd012@gmail.com>
2024-08-23 21:40:22 -0400

    Formatting.

--- src/background/background.ts
+++ src/background/background.ts
@@ -4,31 +4,31 @@ const CONTEXTS = ['selection'] as chrome.contextMenus.ContextType[];
 const YOUTUBE_SEARCH_URL = 'https://www.youtube.com/results?search_query=';
 
 function createContextMenu(): void {
-  chrome.contextMenus.create({
-    id: CONTEXT_MENU_ID,
-    title: CONTEXT_MENU_TITLE,
-    contexts: CONTEXTS,
-  }, () => {
-    if (chrome.runtime.lastError) {
-      console.error('Error creating context menu:', chrome.runtime.lastError);
-    }
-  });
+    chrome.contextMenus.create({
+        id: CONTEXT_MENU_ID,
+        title: CONTEXT_MENU_TITLE,
+        contexts: CONTEXTS,
+    }, () => {
+        if (chrome.runtime.lastError) {
+            console.error('Error creating context menu:', chrome.runtime.lastError);
+        }
+    });
 }
 
 function generateYouTubeSearchURL(encodedQuery: string): string {
-  return `${YOUTUBE_SEARCH_URL}${encodedQuery}`;
+    return `${YOUTUBE_SEARCH_URL}${encodedQuery}`;
 }
 
 function openNewTab(url: string): void {
-  chrome.tabs.create({ url })
-    .catch(error => console.error('Error creating tab:', error));
+    chrome.tabs.create({url})
+        .catch(error => console.error('Error creating tab:', error));
 }
 
 function handleContextMenuClick(info: chrome.contextMenus.OnClickData): void {
-  if (info.menuItemId === CONTEXT_MENU_ID && info.selectionText) {
-    const youtubeSearchUrl = generateYouTubeSearchURL(info.selectionText);
-    openNewTab(youtubeSearchUrl);
-  }
+    if (info.menuItemId === CONTEXT_MENU_ID && info.selectionText) {
+        const youtubeSearchUrl = generateYouTubeSearchURL(info.selectionText);
+        openNewTab(youtubeSearchUrl);
+    }
 }
 
 chrome.runtime.onInstalled.addListener(createContextMenu);
