de13a322aa54f96d75314e1ecb861e68741e40ce
Wael Hamdan <whamd012@gmail.com>
2024-09-02 20:43:43 -0400

    Arrow functions.

--- src/background/background.ts
+++ src/background/background.ts
@@ -3,40 +3,36 @@ const CONTEXT_MENU_TITLE = "Search YouTube for '%s'";
 const CONTEXTS = ['selection'] as chrome.contextMenus.ContextType[];
 const YOUTUBE_SEARCH_URL = 'https://www.youtube.com/results?search_query=';
 
-function createContextMenu(): void {
+const createContextMenu = (): void => {
     const contextMenuConfig = getContextMenuConfig();
-    chrome.contextMenus.create(contextMenuConfig, handleRuntimeError);
-}
+    chrome.contextMenus.create(contextMenuConfig, () => {
+        if (chrome.runtime.lastError) {
+            console.error('Error creating context menu:', chrome.runtime.lastError);
+        }
+    });
+};
 
-function handleContextMenuClick(info: chrome.contextMenus.OnClickData): void {
+const handleContextMenuClick = (info: chrome.contextMenus.OnClickData): void => {
     if (info.menuItemId === CONTEXT_MENU_ID && info.selectionText) {
         const youtubeSearchUrl = generateYouTubeSearchURL(info.selectionText);
         openNewTab(youtubeSearchUrl);
     }
-}
+};
 
-function openNewTab(url: string): void {
+const openNewTab = (url: string): void => {
     chrome.tabs.create({ url }).catch((error) => console.error('Error creating tab:', error));
-}
+};
 
-function generateYouTubeSearchURL(query: string): string {
+const generateYouTubeSearchURL = (query: string): string => {
     const encodedQuery = encodeURIComponent(query);
     return `${YOUTUBE_SEARCH_URL}${encodedQuery}`;
-}
+};
 
-function getContextMenuConfig(): chrome.contextMenus.CreateProperties {
-    return {
-        id: CONTEXT_MENU_ID,
-        title: CONTEXT_MENU_TITLE,
-        contexts: CONTEXTS,
-    };
-}
-
-function handleRuntimeError(): void {
-    if (chrome.runtime.lastError) {
-        console.error('Error creating context menu:', chrome.runtime.lastError);
-    }
-}
+const getContextMenuConfig = (): chrome.contextMenus.CreateProperties => ({
+    id: CONTEXT_MENU_ID,
+    title: CONTEXT_MENU_TITLE,
+    contexts: CONTEXTS,
+});
 
 chrome.runtime.onInstalled.addListener(createContextMenu);
 chrome.contextMenus.onClicked.addListener(handleContextMenuClick);
